8cb2d5ab316fd812f15d1bae10f547be42af5702
Magnus Widen <magnus@thebrewery.se>
2017-09-05 16:34:50 +0200

    starting on user details route

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ app.get( '/users', function ( req, res ) {
     var buffer = '<h1>All my Users</h1>';
     User.find( {}, function ( err, users ) {
         users.forEach( function ( user ) {
-            buffer += '<a href="/users/'+user.username+'">' + user.name.full + '</a><br>';
+            buffer += '<a href="/users/' + user.username + '">' + user.name.full + '</a><br>';
         } );
         res.send( buffer );
     } );
 } );
 
-app.get('/users/:username', function(req, res){
-
-    res.send(req.params.username);
-})
+app.get( '/users/:username', function ( req, res ) {
+    var username = req.params.username;
+    var buffer = "";
+    User.findOne( { username: username }, function ( err, user ) {
+        buffer += '<h1>' + user.name.full + '</h1>';
+        res.send( buffer );
+    } );
+} )
 
 var server = app.listen( 4000, function () {
     console.log( 'server running at http://localhost:' + server.address().port );
