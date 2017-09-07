923696a21c85be49c3903722adc6d6fdd2ac16d1
Magnus Widen <magnus@thebrewery.se>
2017-09-07 16:00:34 +0200

    update, delete user, bootstrap sass added

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var app = express();
 var engines = require( 'consolidate' );
 var sassMiddleware = require( 'node-sass-middleware' );
 var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
-var User = require( './db' ).User;
+var routes = require( './routes' );
 
 app.engine( 'hbs', engines.handlebars );
 app.set( 'views', './views' );
@@ -15,37 +14,15 @@ app.use( sassMiddleware( {
     dest: path.join( __dirname, '/public/' ),
     debug: true,
     outputStyle: 'compressed',
-    prefix: '/css'
+    prefix: '/public'
 } ) );
-// Note: you must place sass-middleware *before* `express.static` or else it will
-// not work.
+
+app.use( '/', routes );
 app.use( '/public', express.static( path.join( __dirname, 'public' ) ) );
-app.use( '/profilepics', express.static( 'public/images' ) );
+app.use( '/fonts', express.static( 'public/fonts' ) );
 
-/* SERVER ROUTES */
-app.get( '/', function ( req, res ) {
-    res.render( 'index', {} );
-} );
 
-app.get( '/users', function ( req, res ) {
-    User.find( {}, function ( err, users ) {
-        // users.forEach( function ( user ) {
-        //     buffer += '<a href="/users/' + user.username + '">' + user.name.full + '</a><br>';
-        // } );
-        //res.send( buffer );
-        res.render( 'users', { users: users } )
-    } );
-} );
 
-app.get( '/users/:username', function ( req, res ) {
-    var username = req.params.username;
-    User.findOne( { username: username }, function ( err, user ) {
-        //buffer += '<h1>' + user.name.full + '</h1>';
-        //res.send( user );
-        console.log( typeof user.name.full, typeof user.email );
-        res.render( 'user-details', { user: user } );
-    } );
-} )
 
 var server = app.listen( 4000, function () {
     console.log( 'server running at http://localhost:' + server.address().port );
---
+++ routes.js
@@ -0,0 +1,78 @@
+var express = require( 'express' );
+var router = express();
+var User = require( './db' ).User;
+var _ = require( 'lodash' );
+var bodyParser = require( 'body-parser' );
+var methodOverride = require( 'method-override' );
+
+router.use( bodyParser.urlencoded( { extended: true } ) );
+router.use( methodOverride( function ( req, res ) {
+    if ( req.body && typeof req.body === 'object' && '_method' in req.body ) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+} ) )
+
+/* SERVER ROUTES */
+router.get( '/', function ( req, res ) {
+    res.render( 'index', {} );
+} );
+
+// GET ALL USERS AND DISPLAY THEM I A LIST VIEW
+router.get( '/users', function ( req, res ) {
+    User.find( {}, function ( err, users ) {
+        res.render( 'users', { users: users } )
+    } );
+} );
+
+// GET SPECIFIC USER AND DISPLAY DETAILS PAGE
+router.get( '/users/:username', function ( req, res ) {
+    var username = req.params.username;
+    User.findOne( { username: username }, function ( err, user ) {
+        res.render( 'user-details', { user: user } );
+    } );
+} );
+
+// EDIT SPECIFIC USER VIEW
+router.get( '/users/:username/edit', function ( req, res ) {
+    var username = req.params.username;
+    User.findOne( { username: username }, function ( err, user ) {
+        res.render( 'user-edit', { user: user } );
+    } );
+} );
+
+router.put( '/users/:username/edit', function ( req, res ) {
+    var username = req.params.username;
+    var updateObject = {
+        email: req.body.email,
+        cell: req.body.cell,
+        salt: req.body.salt,
+        name: {
+            first: req.body.first,
+            last: req.body.last,
+            full: _.startCase( req.body.first + " " + req.body.last ),
+            title: req.body.title
+        }
+    }
+    User.findOneAndUpdate( { username: username }, updateObject, function ( err, user ) {
+        res.redirect( '/users/' + username );
+    } );
+} );
+
+router.delete( '/users/:username/remove', function ( req, res ) {
+    var username = req.params.username;
+    User.deleteOne( { username: username }, function ( error, writeOpResult ) {
+        res.redirect( '/users' );
+    } );
+} );
+
+router.get( '/raw/:username', function ( req, res ) {
+    var username = req.params.username;
+    User.findOne( { username: username }, function ( err, user ) {
+        res.send( user );
+    } );
+} );
+
+module.exports = router;
